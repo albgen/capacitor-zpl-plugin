02ab5496530b66424af09ff9033026508fba8dd7
evald80 <evald80@gmail.com>
2025-02-21 11:59:42 +0100

    feat: add ES module support and printer management methods
    - Add module type specification to enable ES module syntax.
    - Add Bluetooth permission checks and new printer management methods to the ZebraPrinterPlugin interface.

--- src/definitions.ts
+++ src/definitions.ts
@@ -11,9 +11,31 @@ export interface ZebraPrinterPlugin {
    * @param options Contains the ZPL code and printer ID.
    */
   print(options: { zpl: string; printerId: string }): Promise<void>;
+
+  bluetoothHasPermissions(): Promise<{result: boolean;}>;
+  bluetoothIsEnabled(): Promise<{result: boolean;}>;
+  listPrinters(options: {type: string;}): Promise<Printers>;
+  printFormattedText(options: { type: string; id: string; address?: string; port?: string; action?: string; text: string, mmFeedPaper?:String, useEscPosAsterik?: boolean, initializeBeforeSend?: boolean, sendDelay?: string; chunkSize?: string}): Promise<void>;
+  logCat(options: { message: string; }): Promise<void>;
+  rejectTest(): Promise<void>;
+  throwException(): Promise<void>;
+  echo(options: {value: string;}): Promise<{value: string;}>;
 }
 
 export interface Printer {
   id: string;    // Unique identifier (e.g., Bluetooth address)
   name: string;  // Friendly name of the printer
+
+  address: string;
+  bondState: string;
+  type: string;
+  //features: string;
+  deviceClass: string;
+  majorDeviceClass: string;
+  //[uuid: string]: string; // For dynamic UUID keys
+}
+
+export interface Printers
+{
+  [key: string]: Printer;  // Dynamic key, where key is the printer name found also on name property of the PrinterInfo
 }
