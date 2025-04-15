270574a53b75903a5aefb94e5b82a653d8383c45
evald80 <evald80@gmail.com>
2025-04-15 11:41:28 +0200

    chore: Update import paths with file extensions.
    - Modify import paths to include file extensions for web module and definitions.

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import { registerPlugin } from '@capacitor/core';
 import type { ZebraPrinterPlugin } from './definitions';
 
 const ZebraPrinter = registerPlugin<ZebraPrinterPlugin>('ZebraPrinter', {
-  web: () => import('./web').then(m => new m.ZebraPrinterWeb()),
+  web: () => import('./web.js').then(m => new m.ZebraPrinterWeb()),
 });
 
-export * from './definitions';
+export * from './definitions.js';
 export { ZebraPrinter };
